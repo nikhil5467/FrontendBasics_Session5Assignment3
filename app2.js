28f86d6ba9e775045bcd3547ebf79265f7985bea
nikhil5467 <nikhilrocks374@gmail.com>
2016-09-14 00:24:41 +0530

    session5assignment3

---
+++ app2.js
@@ -0,0 +1,19 @@
+var employee ={
+    
+    name : "Nikhil Kumar",
+    age : 25,
+    salary : 30000,
+    address :{
+        
+        city : "Bengaluru",
+        state : "Karnataka",
+        pincode : 564100
+    }
+}
+
+console.log("Name :"+employee.name);
+console.log("Age :"+employee.age);
+console.log("Salary :"+employee.salary);
+console.log("City :"+employee.address.city);
+console.log("State :"+employee.address.state);
+console.log("Pincode :"+employee.address.pincode);
